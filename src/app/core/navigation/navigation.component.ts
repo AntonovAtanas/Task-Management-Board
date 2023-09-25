79220dff1170a45fdcb592d5f1dd516d5f7b6572
AntonovAtanas <atanas.antonov1@gmail.com>
2023-09-25 18:44:12 +0300

    Style the popup for add task

--- src/app/core/navigation/navigation.component.ts
+++ src/app/core/navigation/navigation.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+
+import { AddTaskComponent } from 'src/app/shared/add-task/add-task.component';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatDialogModule
+  ],
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent {
 
+  constructor(private matDialog: MatDialog){}
+
+  addTask() {
+    this.matDialog.open(AddTaskComponent, {
+      panelClass: 'mdc-dialog__surface'
+    })
+  }
 }
