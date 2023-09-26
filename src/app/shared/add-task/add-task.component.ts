3101ab3afb3c9485cf4f10aad6f4d09f354a4061
AntonovAtanas <atanas.antonov1@gmail.com>
2023-09-26 10:44:11 +0300

    Set new tasks in the todo localstorage

--- src/app/shared/add-task/add-task.component.ts
+++ src/app/shared/add-task/add-task.component.ts
@@ -1,33 +1,45 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import  { v4 as uuid} from 'uuid';
+import { v4 as uuid } from 'uuid';
 
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
 import { NgForm } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
+import { Task } from 'src/app/interfaces/Task';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [
-    CommonModule,
-    FormsModule
-  ],
+  imports: [CommonModule, FormsModule],
   templateUrl: './add-task.component.html',
-  styleUrls: ['./add-task.component.css']
+  styleUrls: ['./add-task.component.css'],
 })
 export class AddTaskComponent {
-
-  constructor(private matDialogReference: MatDialog){}
+  constructor(private matDialogReference: MatDialog) {}
 
   onCreate(createForm: NgForm) {
 
-    console.log(createForm.value)
+    let userTask = createForm.value.task;
 
-    this.matDialogReference.closeAll()
+    const newTask: Task = {
+      task: userTask,
+      _taskId: this.generateId()
+    }
+
+    this.setTask(newTask);
+
+    this.matDialogReference.closeAll();
   }
 
-  generateId() {
+  generateId(): string {
     return uuid();
+  };
+
+  setTask(newTask: Task): void {
+    let todoTasks: Task[] = JSON.parse(localStorage.getItem('todo') || '[]');
+
+    todoTasks.push(newTask);
+
+    localStorage.setItem('todo', JSON.stringify(todoTasks));
   }
 }
