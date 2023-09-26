7afd91dc83446a9d31c39c7722662316bdbbf525
AntonovAtanas <atanas.antonov1@gmail.com>
2023-09-26 10:03:47 +0300

    Add submit form handler

--- src/app/shared/add-task/add-task.component.ts
+++ src/app/shared/add-task/add-task.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import  { v4 as uuid} from 'uuid';
+
+import { FormsModule } from '@angular/forms'
+import { NgForm } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './add-task.component.html',
   styleUrls: ['./add-task.component.css']
 })
 export class AddTaskComponent {
 
+  constructor(private matDialogReference: MatDialog){}
+
+  onCreate(createForm: NgForm) {
+
+    console.log(createForm.value)
+
+    this.matDialogReference.closeAll()
+  }
+
+  generateId() {
+    return uuid();
+  }
 }
